fd87434446367c7205f2c7b875bea9d23efb134c
Sunshine <yewhang2004@gmail.com>
2025-11-27 06:31:13 +0800

    Add stack library to data_structure/.

---
+++ data_structure/stack.c
@@ -0,0 +1,26 @@
+#include "stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Stack* createStack(){
+    Stack* newStack = malloc(sizeof(Stack));
+
+    newStack -> next = NULL;
+    newStack -> previous = NULL;
+    newStack -> data = NULL;
+
+    return newStack;
+}
+
+void pushStack(Stack* stack, size_t elementSize, void** elementPointer){
+    linkedListAddNext(stack, elementSize, elementPointer);
+}
+
+void popStack(Stack* stack, void** elementPointer){
+    if (stack -> next == NULL){
+        *elementPointer = NULL;
+    }else {
+        *elementPointer = stack -> next -> data;
+        stack -> next = stack -> next -> next;
+    }
+}
