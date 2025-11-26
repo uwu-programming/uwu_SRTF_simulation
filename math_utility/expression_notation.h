62377afad8641984b015cf25ffdef30f30b32a6b
Sunshine <yewhang2004@gmail.com>
2025-11-26 08:35:36 +0800

    Create .h and .c files for expression notation conversion function.

---
+++ math_utility/expression_notation.h
@@ -0,0 +1,9 @@
+// equation_notation.h
+// A library that help converts mathematic expression from one notation type to another.
+
+#ifndef __EXPRESSION_NOTATION__
+#define __EXPRESSION_NOTATION__
+
+
+
+#endif
