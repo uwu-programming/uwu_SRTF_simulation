84d8734390c06a2c5f4de55a9bdd3530a08fd7ab
Sunshine <yewhang2004@gmail.com>
2025-12-07 13:39:19 +0800

    Complete basic UI structure for processSection - the window that show process list and allow adding new process.

--- simulator/main.c
+++ simulator/main.c
@@ -8,6 +8,12 @@
 
 static GtkWidget* window;
 static GtkBuilder* builder;
+static GtkCssProvider* cssProvider;
+
+struct SimulatorUIController{
+    GtkScrolledWindow* processListWindow;
+    GtkListBox* processList;
+}uiController;
 
 static ProcessScheduler* simulatorProcessScheduler;
 static int processorAmount = 0;
@@ -24,7 +30,13 @@ void resetSimulator(){
 }
 
 void initializeSimulator(){
+    uiController.processListWindow = GTK_SCROLLED_WINDOW(gtk_builder_get_object(builder, "processListWindow"));
+    uiController.processList = GTK_LIST_BOX(gtk_builder_get_object(builder, "processList"));
+
     simulatorProcessScheduler = createProcessScheduler();
+    for (int i = 0; i < 20; i++){
+        gtk_list_box_append(uiController.processList, gtk_button_new_with_label("hewwo"));
+    }
 }
 
 static void activate(GtkApplication* app, gpointer user_data){
@@ -36,7 +48,12 @@ static void activate(GtkApplication* app, gpointer user_data){
     gtk_builder_add_from_file(builder, "simulator_ui.ui", NULL);
     GObject* windowContent = gtk_builder_get_object(builder, "windowContent");
     gtk_window_set_child(GTK_WINDOW(window), GTK_WIDGET(windowContent));
-    
+    cssProvider = gtk_css_provider_new();
+    gtk_css_provider_load_from_path(cssProvider, "simulator_ui.css");
+    gtk_style_context_add_provider_for_display(gdk_display_get_default(), GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+
+    initializeSimulator();
+
     gtk_window_present(GTK_WINDOW(window));
 }
 
